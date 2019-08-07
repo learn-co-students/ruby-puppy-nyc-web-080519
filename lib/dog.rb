42e14a7e576b2d14f2d33d0e60c2b51ea16688fa
github username <github email address>
2019-08-07 00:46:16 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog
+    @@all = [] 
+    
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def name
+        @name
+    end
+    
+    def save
+        @@all << self 
+    end    
+    
+    def self.all #called on the class self. NOT a method of the instance => GPS to all the BMWs. To call this it would be Dog.all. @@ keeps tracks of all the dogs/BMWs that were ever created. 
+        @@all
+    end
+    
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+    
+    def self.clear_all
+        @@all.clear
+    end
+end
+
